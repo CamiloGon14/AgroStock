a0d31f9720448ff8212bc4b0824042c6bf6e3cb2
JCGonzalez <johancamilogonzalez@ucundinamarca.edu.co>
2026-04-11 16:07:16 -0500

    Creacion de Auth, definicion de Roles y creacion del CRUD productos

---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Producto;
+use Illuminate\Http\Request;
+
+class ProductoController extends Controller
+{
+public function index() {
+    $productos = Producto::all();
+    return view('productos.index', compact('productos'));
+}
+
+public function create() {
+    return view('productos.create');
+}
+
+public function store(Request $request) {
+    $validatedData = $request->validate([
+        'nombre' => 'required|string|max:255',
+        'precio' => 'required|numeric',
+        'descripcion' => 'nullable|string',
+    ]);
+    Producto::create($validatedData);
+    return redirect()->route('productos.index');
+}
+
+public function edit(Producto $producto) {
+    return view('productos.edit', compact('producto'));
+}
+
+public function update(Request $request, Producto $producto) {
+    $producto->update($request->all());
+    return redirect()->route('productos.index');
+}
+
+public function destroy(Producto $producto) {
+    $producto->delete();
+    return redirect()->route('productos.index');
+}
+}
