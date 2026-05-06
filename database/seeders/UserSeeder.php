c1171a62c5f7dae07f8e139433e1eb511cc7293e
JCGonzalez <johancamilogonzalez@ucundinamarca.edu.co>
2026-05-05 21:48:49 -0500

    Conflicto resuelto en UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        // Asegurar que el rol admin existe
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+
+        // Usuario Alejo
+        $alejo = User::create([
+            'name' => 'Alejo',
+            'email' => 'alejo@AgroStock.com',
+            'password' => Hash::make('12345678')
+        ]);
+
+        $alejo->assignRole($adminRole);
+
+        // Usuario Camilo
+        $camilo = User::create([
+            'name' => 'Camilo',
+            'email' => 'camilo@AgroStock.com',
+            'password' => Hash::make('12345678')
+        ]);
+
+        $camilo->assignRole($adminRole);
+    }
+}
