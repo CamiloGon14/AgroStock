88a1c1b97961543d2cac9592959f24dfe3b5b156
JCGonzalez <johancamilogonzalez@ucundinamarca.edu.co>
2026-05-20 08:42:03 -0500

    Correccion de errores

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Spatie\Permissions\Traits\HasRoles;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
